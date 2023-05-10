6ae559c77153f5e29960687fc882e17dd726e23d
jakubgrela2003 <133110118+jakubgrela2003@users.noreply.github.com>
2023-05-10 12:54:21 +0100

    Add files via upload
    Added 5 code examples TSE 3

---
+++ CSharp Code Examples/Basics/HeapSort.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharp_Code_Examples
+
+
+class HeapSort
+{
+    public static void run()
+    //Authors: Jakub Grela
+    //A heapsort which asks for user input, uses the sort and displays output to console
+    {
+        // Ask the user to input 5 numbers
+        Console.WriteLine("Please enter 5 numbers:");
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"Number {i + 1}: ");
+            if (!int.TryParse(Console.ReadLine(), out numbers[i]))
+            {
+                Console.WriteLine("Invalid input. Please enter an integer.");
+                i--;
+            }
+        }
+
+        // Sort the numbers using heap sort
+        HeapSort(numbers);
+
+        // Output the sorted numbers to the console
+        Console.WriteLine("Sorted numbers:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.ReadLine();
+    }
+
+    static void HeapSort(int[] array)
+    {
+        // Build a max heap from the array
+        for (int i = array.Length / 2 - 1; i >= 0; i--)
+        {
+            Heapify(array, array.Length, i);
+        }
+
+        // Heap sort
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Swap(array, 0, i);
+            Heapify(array, i, 0);
+        }
+    }
+
+    static void Heapify(int[] array, int n, int i)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest])
+        {
+            largest = left;
+        }
+
+        if (right < n && array[right] > array[largest])
+        {
+            largest = right;
+        }
+
+        if (largest != i)
+        {
+            Swap(array, i, largest);
+            Heapify(array, n, largest);
+        }
+    }
+
+    static void Swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
---
+++ CSharp Code Examples/Basics/InsertionSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Code_Examples
+
+class InsertionSort
+{
+    public static void run()
+    // Authors: Jakub Grela
+    //An insertion sort which asks the user for numbers and then sorts them with output to the console
+    {
+        // Ask the user to enter a list of integers separated by spaces
+        Console.WriteLine("Enter a list of integers separated by spaces:");
+        var input = Console.ReadLine();
+
+        // Convert the input to an array of integers
+        var nums = Array.ConvertAll(input.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+        // Print the unsorted list
+        Console.WriteLine("Unsorted list:");
+        Console.WriteLine(string.Join(", ", nums));
+
+        // Sort the array using insertion sort
+        for (int i = 1; i < nums.Length; i++)
+        {
+            int j = i;
+            while (j > 0 && nums[j - 1] > nums[j])
+            {
+                // Swap the elements at positions j and j - 1
+                int temp = nums[j - 1];
+                nums[j - 1] = nums[j];
+                nums[j] = temp;
+                j--;
+
+                // Provide feedback on the sorting process
+                Console.WriteLine($"Sorting... {string.Join(", ", nums)}");
+            }
+        }
+
+        // Print the sorted list
+        Console.WriteLine("Sorted list:");
+        Console.WriteLine(string.Join(", ", nums));
+
+        // Wait for user input before closing the program
+        Console.ReadLine();
+    }
+}
